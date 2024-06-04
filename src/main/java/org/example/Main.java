23d8887abe2a56b8e0d9d1f17022a46598dbf613
Анастасия Шахно <nastyashakhno@gmail.com>
2024-06-04 16:51:30 +0300

    слава богу заработало, добавить защиту от умных капч

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class Main {
+    @FindBy(xpath = "//span[contains(text(),'Каталог')]")
+    public static WebElement catalogButton;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(catalogButton.getText());
     }
 }
